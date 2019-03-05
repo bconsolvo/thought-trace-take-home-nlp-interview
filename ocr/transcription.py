bdaea68d3a432f83eee4b6aa0034f954e6b2318a
Benjamin Consolvo <bconsolvo@users.noreply.github.com>
2019-03-05 13:19:48 -0600

    Update transcription.py

--- ocr/transcription.py
+++ ocr/transcription.py
@@ -17,7 +17,9 @@ class ocr_pytesseract:
             print('Transcribing text for,' i)
             text = pytesseract.image_to_string(imjpg) # Uses trained model from Tesseract to convert JPG image to a string of text
             scores = pytesseract.image_to_data(imjpg, output_type=pytesseract.Output.DATAFRAME) # scores for each word transcribed
-            # text_conf = scores[['text','conf']] # Extracting only the text and associated scores dataframe
+            #text_conf = scores[['text','conf']] # Extracting only the text and associated scores dataframe
             mean1 = scores.conf.replace(-1,np.NaN).mean() # Taking the mean confidence value over each word in the transcription
             print('The mean confidence value of', i, 'is', mean1,'. Transcription complete!')
             lst_text.append(text)
+    return lst_text, scores 
+    
