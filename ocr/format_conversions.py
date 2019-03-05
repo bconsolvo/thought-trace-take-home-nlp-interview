93d4ae4bb52e4b00b58eeff35516b7096c3d3f5a
Benjamin Consolvo <bconsolvo@users.noreply.github.com>
2019-03-05 12:28:58 -0600

    Update and rename pdf2jpg.py to format_conversions.py

---
+++ ocr/format_conversions.py
@@ -0,0 +1,7 @@
+from wand.image import Image as Img
+
+def pdf2png(pdffile):
+    new_title = pdffile.split('.')[-2] + '.jpg'
+    with Img(filename=pdffile, resolution=300) as img:
+        img.compression_quality = 99
+        img.save(filename=new_title)
--- ocr/pdf2jpg.py
+++
@@ -1,3 +0,0 @@
-with Img(filename='doc_test.pdf', resolution=300) as img:
-    img.compression_quality = 99
-    img.save(filename='doc_test.jpg')
