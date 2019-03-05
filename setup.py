b3509662f36a90cd2c39258baa6354b033437b30
Benjamin Consolvo <bconsolvo@users.noreply.github.com>
2019-03-05 13:47:42 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'Topic :: Software DEvelopment :: Build Tools',
+        'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
     ],
