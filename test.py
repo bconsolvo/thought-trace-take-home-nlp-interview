b4cd99c7984a9ff1a24676701f2d81dc266076d4
Benjamin Consolvo <bconsolvo@users.noreply.github.com>
2019-03-06 12:38:22 -0600

    Update test.py

--- test.py
+++ test.py
@@ -7,16 +7,16 @@ from src import formatconversions
 from src.transcription import ocrpytesseract
 from src import conftranscription
 
-# formatconversions.pdf2png('files/doc_test.pdf')
+# formatconversions.pdf2png('src/doc_test.pdf')
 
 
 
 a = ocrpytesseract()
-txt_strings, score_tbl = a.jpg2text('files/')
+txt_strings, score_tbl = a.jpg2text('src/')
 print(txt_strings)
 print(score_tbl)
 
 
-master,grade = conftranscription.ocrall('files/doc_test_ocr.csv','files/valid_words.txt')
+master,grade = conftranscription.ocrall('src/doc_test_ocr.csv','src/valid_words.txt')
 print(master)
 print(grade)
